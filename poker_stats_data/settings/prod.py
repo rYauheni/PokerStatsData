889cbcadfae77d130afc473374e0553a43f02b76
rYauheni <ra.yauheni@gmail.com>
2023-07-17 12:43:04 +0300

    fixed settings files (base.py, prod.py)

--- poker_stats_data/settings/prod.py
+++ poker_stats_data/settings/prod.py
@@ -1,11 +1,7 @@
-import os
-
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS += ['localhost', '127.0.0.1', os.getenv('DB_HOST'), '52.232.2.252:5432', 'dumbo.db.elephantsql.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
